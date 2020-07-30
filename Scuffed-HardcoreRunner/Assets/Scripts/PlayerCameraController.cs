b57a7e50b39f4ceeb5ec3fb42688dd3220a4f390
moritz <zocken223344@gmx.de>
2020-07-30 12:59:31 +0200

    Added Basic Movement
    Added Basic W,A,S,D + Jumping Movement and player rotation with camera (PlayerCameraController)

---
+++ Scuffed-HardcoreRunner/Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMovement();
+    }
+
+    public void CameraMovement()
+    {
+        //Mouse Movement Left and Right
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        //Mouse Movement up and Down
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        //Prevent the Camera of flipping around and make sure the Camera follows the Player
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+}
---
+++ Scuffed-HardcoreRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed = 6;
+
+    public Vector3 startPos;
+
+    public bool isGrounded;
+    public Vector3 jump;
+    public float JumpForce = 2.0f;
+
+    public GameObject PlayerCamera;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        if (this.isLocalPlayer)
+        {
+            PlayerCamera.SetActive(true);
+            rb = GetComponent<Rigidbody>();
+            jump = new Vector3(0, 2.0f, 0);
+        }
+        else
+        {
+            PlayerCamera.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+    /// <summary>
+    /// Basic Player Movement
+    /// </summary>
+    public void PlayerMovement()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        if (this.isLocalPlayer)
+        {
+            Vector3 playerMovement = new Vector3(hor, 0, ver) * speed * Time.deltaTime;
+            transform.Translate(playerMovement, Space.Self);
+            //Basic Jump
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                rb.AddForce(jump * JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
